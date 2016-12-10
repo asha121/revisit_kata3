dd1a58bd3041c0dec4be41ff4e1b6b779096a165
Test developer <test@testmail.com>
2016-12-10 11:59:44 +0530

    GREEN

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -3,7 +3,7 @@ package com.cleancode.tdd;
 public class PositiveIntegers {
 
 	public String inWords(int input) {
-		String[] word = { null, "ONE ", "TWO ","THREE","FOUR ","FIVE ","SIX ","SEVEN ","EIGHT ","NINE " };
+		String[] word = { null, "ONE ", "TWO ","THREE","FOUR ","FIVE ","SIX ","SEVEN ","EIGHT ","NINE ","TEN " };
 		return word[input];
 	}
 
