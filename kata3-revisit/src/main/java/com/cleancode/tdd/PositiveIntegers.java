e03caf42c4e4a3b087943310d0b17967e9844ed6
Test developer <test@testmail.com>
2016-12-10 12:12:20 +0530

    REFACTOR >>> FOR NULL CONDITION

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -3,7 +3,7 @@ package com.cleancode.tdd;
 public class PositiveIntegers {
 
 	public String inWords(int input) {
-		String[] word = { null, "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ", "EIGHT ", "NINE ", "TEN ",
+		String[] word = { "", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ", "EIGHT ", "NINE ", "TEN ",
 				"ELEVEN ", "TWELVE ", "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ", "EIGHTEEN ",
 				"NINETEEN " };
 		return word[input];
