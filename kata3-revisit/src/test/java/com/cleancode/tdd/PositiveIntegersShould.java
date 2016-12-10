843a647927ddf99f6e228698705e123d014c16ec
Test developer <test@testmail.com>
2016-12-10 12:33:37 +0530

    REFACTOR  ...

--- kata3-revisit/src/test/java/com/cleancode/tdd/PositiveIntegersShould.java
+++ kata3-revisit/src/test/java/com/cleancode/tdd/PositiveIntegersShould.java
@@ -10,24 +10,43 @@ public class PositiveIntegersShould {
 	public void display_1_as_one() {
 		assertEquals("ONE ", new PositiveIntegers().inWords(1));
 	}
+
 	@Test
 	public void display_2_as_two() {
 		assertEquals("TWO ", new PositiveIntegers().inWords(2));
 	}
+
 	@Test
 	public void display_9_as_nine() {
 		assertEquals("NINE ", new PositiveIntegers().inWords(9));
 	}
+
 	@Test
 	public void display_10_as_ten() {
 		assertEquals("TEN ", new PositiveIntegers().inWords(10));
 	}
+
 	@Test
 	public void display_19_as_ninteen() {
 		assertEquals("NINETEEN ", new PositiveIntegers().inWords(19));
 	}
+
 	@Test
 	public void display_20_as_twenty() {
 		assertEquals("TWENTY ", new PositiveIntegers().inWords(20));
+	}
+
+	@Test
+	public void display_from_11_to_18() {
+		int[] range = { 11, 12, 13, 14, 15, 16, 17, 18 };
+		String[] word = { "ELEVEN ", "TWELVE ", "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ",
+				"EIGHTEEN "};
+		String[] expected = new String[8];
+		int index = 0;
+		for (int input : range) {
+			expected[index] = new PositiveIntegers().inWords(input);
+			index = index + 1;
+		}
+		assertEquals(word, expected);
 	}
 }
