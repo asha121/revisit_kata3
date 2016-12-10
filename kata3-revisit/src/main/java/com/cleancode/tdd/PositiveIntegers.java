ee65c73e5e5713d46675d41e328e2a1af3ab8820
Test developer <test@testmail.com>
2016-12-10 11:30:18 +0530

    GREEN >>   passing test case for 1

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -2,9 +2,8 @@ package com.cleancode.tdd;
 
 public class PositiveIntegers {
 
-	public String inWords(int i) {
-		// TODO Auto-generated method stub
-		return null;
+	public String inWords(int input) {
+		return "ONE ";
 	}
 
 }
