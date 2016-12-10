47323099f66ebf3eb901802f0be9ccc0d0cb17dc
Test developer <test@testmail.com>
2016-12-10 11:45:26 +0530

    REFACTOR >> code refactor for theae test cases

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -3,13 +3,8 @@ package com.cleancode.tdd;
 public class PositiveIntegers {
 
 	public String inWords(int input) {
-		String word = null;
-		if (input == 1) {
-			word = "ONE ";
-		}else if (input == 2) {
-			word = "TWO ";
-		}
-		return word;
+		String[] word = { null, "ONE ", "TWO " };
+		return word[input];
 	}
 
 }
