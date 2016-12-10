eff775e694adaca13656762f636a12db44e05872
Test developer <test@testmail.com>
2016-12-10 11:53:45 +0530

    REFACTOR>>>> pattern generated for the single digit

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -3,7 +3,7 @@ package com.cleancode.tdd;
 public class PositiveIntegers {
 
 	public String inWords(int input) {
-		String[] word = { null, "ONE ", "TWO ","","","","","","","NINE " };
+		String[] word = { null, "ONE ", "TWO ","THREE","FOUR ","FIVE ","SIX ","SEVEN ","EIGHT ","NINE " };
 		return word[input];
 	}
 
