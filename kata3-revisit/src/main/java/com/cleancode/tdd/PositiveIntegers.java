a9c05eea70c34e1c6ae907d56ebb1cc17dce1688
Test developer <test@testmail.com>
2016-12-10 12:48:03 +0530

    REFACTOR

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -3,10 +3,15 @@ package com.cleancode.tdd;
 public class PositiveIntegers {
 
 	public String inWords(int input) {
-		String[] word = { "", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ", "EIGHT ", "NINE ", "TEN ",
+		String[] lessThanTwenty = { "", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ", "EIGHT ", "NINE ", "TEN ",
 				"ELEVEN ", "TWELVE ", "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ", "EIGHTEEN ",
-				"NINETEEN ","TWENTY ","TWENTY ONE ","TWENTY TWO " };
-		return word[input];
+				"NINETEEN "};
+		String[] twentyOnwards = {"TWENTY ","TWENTY ONE ","TWENTY TWO "};
+		if (input<20) {
+			return lessThanTwenty[input];
+		}
+		return twentyOnwards[input%20];
+		
 	}
 
 }
