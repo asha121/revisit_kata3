cab0f108ab162aaa9fdb5e6f5d837d89a435f192
Test developer <test@testmail.com>
2016-12-10 13:48:54 +0530

    GREEN >> 30

--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -6,10 +6,10 @@ public class PositiveIntegers {
 		String[] lessThanTwenty = { "", "ONE ", "TWO ", "THREE ", "FOUR ", "FIVE ", "SIX ", "SEVEN ", "EIGHT ", "NINE ",
 				"TEN ", "ELEVEN ", "TWELVE ", "THIRTEEN ", "FOURTEEN ", "FIFTEEN ", "SIXTEEN ", "SEVENTEEN ",
 				"EIGHTEEN ", "NINETEEN " };
-		String[] twenty = { "", "TWENTY " };
+		String[] twenty = { "","", "TWENTY " ,"THIRTY "};
 		if (input < 20) {
 			return lessThanTwenty[input];
 		}
-		return twenty[input / 20] + lessThanTwenty[input % 20];
+		return twenty[input / 10] + lessThanTwenty[input % 10];
 	}
 }
