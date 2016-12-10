f13bfdf9230b4d7615d8a17d4b17b0a326e3ce5a
Test developer <test@testmail.com>
2016-12-10 21:27:37 +0530

    REFACTOR

--- kata3-revisit/src/main/java/com/cleancode/tdd/NumberFactory.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/NumberFactory.java
@@ -1,22 +1,16 @@
 package com.cleancode.tdd;
 
-import org.yaml.snakeyaml.serializer.NumberAnchorGenerator;
+public enum NumberFactory {
+	LESS_THAN_TWENTy(new NumberLessThanTwenty()), LESS_THAN_HUNDRED(new NumberLessThanHundred()), LESS_THAN_THOUSAND(
+			new NumberLessThanThousand()), GREATER_THAN_THOUSAND(new NumbeGreaterOrThousand());
+	private final PositveNumber instance;
 
-public class NumberFactory {
-	enum Number {
-		LESS_THAN_TWENTy(new NumberLessThanTwenty()), 
-		LESS_THAN_HUNDRED(new NumberLessThanHundred()), 
-		LESS_THAN_THOUSAND(	new NumberLessThanThousand()), 
-		GREATER_THAN_THOUSAND(new NumbeGreaterOrThousand());
-		private final PositveNumber instance;
-
-		private Number(PositveNumber instance) {
-			this.instance = instance;
-		}
-
-		public PositveNumber getInstance() {
-			return this.instance;
-		}
-
+	private NumberFactory(PositveNumber instance) {
+		this.instance = instance;
 	}
+
+	public PositveNumber getInstance() {
+		return this.instance;
+	}
+
 }
--- kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/PositiveIntegers.java
@@ -1,20 +1,18 @@
 package com.cleancode.tdd;
 
-import com.cleancode.tdd.NumberFactory.Number;
-
 public class PositiveIntegers {
 
 	public String inWords(int input) {
 		String words = "";
 		if (input < 20) {
-			words = Number.LESS_THAN_TWENTy.getInstance().inWords(input);
+			words = NumberFactory.LESS_THAN_TWENTy.getInstance().inWords(input);
 		} else if (input < 100) {
-			words = Number.LESS_THAN_HUNDRED.getInstance().inWords(input);
+			words = NumberFactory.LESS_THAN_HUNDRED.getInstance().inWords(input);
 		} else if (input < 1000) {
-			words = Number.LESS_THAN_THOUSAND.getInstance().inWords(input);
+			words = NumberFactory.LESS_THAN_THOUSAND.getInstance().inWords(input);
 			;
 		} else {
-			words = Number.GREATER_THAN_THOUSAND.getInstance().inWords(input);
+			words = NumberFactory.GREATER_THAN_THOUSAND.getInstance().inWords(input);
 		}
 
 		return words;
