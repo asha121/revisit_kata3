37442b88e384f361402c836ccc1f7b88bc8d189c
Test developer <test@testmail.com>
2016-12-10 14:41:22 +0530

    GREEN ....RANGE

--- kata3-revisit/src/main/java/com/cleancode/tdd/NumberLessThanHundred.java
+++ kata3-revisit/src/main/java/com/cleancode/tdd/NumberLessThanHundred.java
@@ -3,7 +3,8 @@ package com.cleancode.tdd;
 public class NumberLessThanHundred extends NumberLessThanTwenty implements PositveNumber {
 	@Override
 	public String inWords(int input) {
-		String[] twenty = { "", "", "TWENTY ", "THIRTY " };
+		String[] twenty = { "", "", "TWENTY ", "THIRTY ", "FORTY ", "FIFTY ", "SIXTY ", "SEVENTY ", "EIGHTY ",
+				"NINETY " };
 		return twenty[input / 10] + super.inWords(input % 10);
 	}
 }
